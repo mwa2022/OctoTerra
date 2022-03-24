e9f328311469df0121bb118bea54243a21dcf1c6
mwa2022 <102241644+mwa2022@users.noreply.github.com>
2022-03-24 10:41:35 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+terraform {
+  required_providers {
+    octopusdeploy = {
+      source = "OctopusDeployLabs/octopusdeploy"
+      version = "version-number" # example: 0.7.62
+    }
+  }
+}
+
+data "octopusdeploy_space" "space" {
+  provider = octopusdeploy.unscoped
+  name     = "SandBox"
+}
+
+provider "octopusdeploy" {
+  # configuration options
+  address    = "https://taaoctodeploy.wolterskluwer.com/"     # (required; string) the service endpoint of the Octopus REST API
+  api_key    = "API-ACI6EEPLFRY00OB3IT8GOWFEPNGXKPSF"               # (required; string) the API key to use with the Octopus REST API
+  space_id   = data.octopusdeploy_space.space.id # (optional; string) the space ID in Octopus Deploy
+}

1726986e5020f7be4b9836a4940ee2db7d046049
mwa2022 <102241644+mwa2022@users.noreply.github.com>
2022-03-24 11:55:24 -0400

    Add files via upload

---
+++ mikewtestproject.tf
@@ -0,0 +1,6 @@
+resource "octopusdeploy_table_name" "tblnameTF" {
+  name = "test1"
+  description = "Table name for SQL query.  This is managed by the Octopus Terraform Provider.  Please do not make changes in the UI, update the TF file instead."
+  
+}
+

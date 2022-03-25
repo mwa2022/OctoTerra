20f67b1f53734f97ff5e0092368fb0fcadf3026d
mwa2022 <102241644+mwa2022@users.noreply.github.com>
2022-03-25 15:41:24 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "octopus_tblname" {
     type = string
 }
 
+variable "octopus_address" {
+	    type = string
+	}
+	
+
+	variable "octopus_api_key" {
+	    type = string
+	}
+	
+
+	variable "octopus_space_id" {
+	    type = string
+	}
+
+

ba83d26744c593c3237a63b48d5890fa064490b9
mwa2022 <102241644+mwa2022@users.noreply.github.com>
2022-03-25 15:08:27 -0400

    Update mikewtestproject.tf

--- mikewtestproject.tf
+++ mikewtestproject.tf
@@ -1,6 +1,7 @@
 resource "octopusdeploy_variable" "tblnameTF" {
   name = "test1"
-  description = "Table name for SQL query.  This is managed by the Octopus Terraform Provider.  Please do not make changes in the UI, update the TF file instead."
+  value = var.octopus_tblname
+  description = "Table name for SQL query.  This is managed by the Octopus Terraform Provider.  Please do not make changes in   the UI, update the TF file instead."
   
 }
 

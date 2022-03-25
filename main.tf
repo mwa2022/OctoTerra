c64e8e9bd127d6ab68f039ee1800c447bce1f646
mwa2022 <102241644+mwa2022@users.noreply.github.com>
2022-03-25 15:48:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,30 @@
 # PROVIDERS
 ##################################################################################
 
+
 terraform {
-  required_providers {
-    octopusdeploy = {
-      source = "OctopusDeployLabs/octopusdeploy"
-      version = ">= 0.7.64" # example: 0.7.62
-    }
-  }
-}
+	  required_providers {
+	    octopusdeploy = {
+	      source = "OctopusDeployLabs/octopusdeploy"
+	      version = ">= 0.7.64" # example: 0.7.62
+	    }
+	  }
+	
+
+	  backend "s3" { }
+	}
+	
+
+	provider "octopusdeploy" {
+	  # configuration options
+	  address    = var.octopus_address
+	  api_key    = var.octopus_api_key
+	  space_id   = var.octopus_space_id
+	}
 
 data "octopusdeploy_space" "space" {
-  provider = octopusdeploy.unscoped
+  provider = octopusdeploy
   name     = "SandBox"
 }
 
-provider "octopusdeploy" {
-  # configuration options
-  address    = "https://taaoctodeploy.wolterskluwer.com/"     # (required; string) the service endpoint of the Octopus REST API
-  api_key    = "API-ACI6EEPLFRY00OB3IT8GOWFEPNGXKPSF"               # (required; string) the API key to use with the Octopus REST API
-  space_id   = data.octopusdeploy_space.space.id # (optional; string) the space ID in Octopus Deploy
-}
+

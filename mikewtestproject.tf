1a2e4fb86a0ecf8b066d43e06e8ef02c59e4c2de
mwa2022 <102241644+mwa2022@users.noreply.github.com>
2022-03-29 12:00:30 -0400

    Delete mikewtestproject.tf

--- mikewtestproject.tf
+++
@@ -1,8 +0,0 @@
-resource "octopusdeploy_variable" "tblnameTF" {
-  name = "test1"
-  type = "String"
-  value = var.octopus_tblname
-  description = "Table name for SQL query.  This is managed by the Octopus Terraform Provider.  Please do not make changes in   the UI, update the TF file instead."
-  
-}
-
